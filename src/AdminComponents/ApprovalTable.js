ead240320691d52060bc07a776d13935d7bb6b7b
Hangry <hangrylydevour@gmail.com>
2021-05-19 00:12:36 +0800

    added non functional approval table - work in progress

--- src/AdminComponents/ApprovalTable.js
+++ src/AdminComponents/ApprovalTable.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import { Table } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Table, Button, Modal } from 'react-bootstrap';
 
-export default function ApprovalTable({ gscs }) {
+export default function ApprovalTable({ gscs, handleApproval }) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <div>
       <Table striped bordered hover variant="dark" responsive>
         <thead id="approval-table-head">
           <tr>
+            <th>#</th>
             <th>Alias</th>
             <th>Height</th>
             <th>Languages</th>
@@ -27,28 +33,54 @@ export default function ApprovalTable({ gscs }) {
         {
           gscs.map((gsc) => {
             return (
-            <tr id="approval-table-body-row">
-              <td>{gsc.alias}</td>
-              <td>{gsc.height}</td>
-              <td>{gsc.languages}</td>
-              <td>{gsc.nationality}</td>
-              <td>{gsc.city}, {gsc.country}</td>
-              <td>Town: {gsc.moving_to_a_different_town}, Country: {gsc.moving_to_a_different_country}</td>
-              <td>{gsc.descriptive_words}</td>
-              <td>{gsc.mbti}, {gsc.enneagram}, {gsc.disc}, {gsc.strengths_finder} </td>
-              <td>{gsc.church_background}</td>
-              <td>{gsc.spiritual_maturity}</td>
-              <td scroll>{gsc.spiritual_gifts}</td>
-              <td scroll>{gsc.reasons_gscf_makes_a_good_partner}</td>
-              <td scroll>{gsc.good_match_for_gscf}</td>
-              <td scroll>{gsc.what_is_important_to_me}</td>
-            </tr>
+              <>
+                <tr onClick={handleShow} key={gsc.id}>
+                  <th>
+                    <Button variant="danger" value={gsc.uuid} onClick={handleApproval}>Approve</Button>
+                  </th>
+                  <td>{gsc.alias}</td>
+                  <td>{gsc.height}</td>
+                  <td>{gsc.languages}</td>
+                  <td>{gsc.nationality}</td>
+                  <td>{gsc.city}, {gsc.country}</td>
+                  <td>Town: {gsc.moving_to_a_different_town}, Country: {gsc.moving_to_a_different_country}</td>
+                  <td>{gsc.descriptive_words}</td>
+                  <td>{gsc.mbti}, {gsc.enneagram}, {gsc.disc}, {gsc.strengths_finder} </td>
+                  <td>{gsc.church_background}</td>
+                  <td>{gsc.spiritual_maturity}</td>
+                  <td>{gsc.spiritual_gifts}</td>
+                  <td>{gsc.reasons_gscf_makes_a_good_partner}</td>
+                  <td>{gsc.good_match_for_gscf}</td>
+                  <td>{gsc.what_is_important_to_me}</td>
+                </tr>
+                <Modal
+                  show={show}
+                  onHide={handleClose}
+                  centered
+                >
+                  <Modal.Header closeButton className="bg-beach">
+                    <Modal.Title className="color-blue">{gsc.alias}</Modal.Title>
+                  </Modal.Header>
+                  <Modal.Body className="bg-beach">
+                    <div className="color-red">Favorite topics/music/movies/books: <span className="color-blue">{gsc.favorite_topics}</span></div>
+                    <div className="color-red">Work: <span className="color-blue">{gsc.do}</span></div>
+                    <div className="color-red">Chill out: <span className="color-blue">{gsc.chill_activites}</span></div>
+                    <div className="color-red">Skills: <span className="color-blue">{gsc.skills_and_talents}</span></div>
+                    <div className="color-red">Area to develop: <span className="color-blue">{gsc.growth_and_development}</span></div>
+                    <div className="color-red">Ever been married, has kids: <span className="color-blue">{gsc.has_been_married_or_has_kids}</span></div>
+                    <div className="color-red">Want kids?: <span className="color-blue">{gsc.want_to_have_kids}</span></div>
+                    <div className="color-red">Something else: <span className="color-blue">{gsc.important_info_to_know}</span></div>
+                  </Modal.Body>
+                  <Modal.Footer className="bg-beach">
+                    <Button variant="secondary" onClick={handleClose}>
+                      Close
+                    </Button>
+                  </Modal.Footer>
+                </Modal>
+              </>
             )
           })
         }
-          <tr>
-            <td></td>
-          </tr>
         </tbody>
       </Table>
     </div>
--- src/AdminPages/AdminApprovalPage.js
+++ src/AdminPages/AdminApprovalPage.js
@@ -5,6 +5,7 @@ import { url } from '../App';
 import "../AdminComponents/AdminApprovalPage.css"
 
 export default function AdminApprovalPage() {
+  const [isLoading, setIsLoading] = useState(false)
   const [gscs, setGscs] = useState([])
 
   useEffect(() => {
@@ -17,10 +18,35 @@ export default function AdminApprovalPage() {
       })
   }, [])
 
+  const handleApproval = e => {
+    let uuid = e.target.value
+    setIsLoading(true)
+
+    axios({
+      method: 'POST',
+      url: `${url}/gscs/status/${uuid}`,
+      data: {
+        is_approved: true,
+        is_active: false
+      }
+    })
+    .then(response => {
+      console.log(response)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+    setIsLoading(false)
+  }
+
   return (
     <div id="approval-page-container">
       <h1>Approval Page</h1>
-      <ApprovalTable gscs={gscs}/>
+      <ApprovalTable
+        gscs={gscs}
+        isLoading={isLoading}
+        handleApproval={handleApproval}
+      />
     </div> 
   )
 }
