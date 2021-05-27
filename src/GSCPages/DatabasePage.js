20ca6462ef8591dcf1c9076fb951d772de7a2cd9
Hangry <hangrylydevour@gmail.com>
2021-05-27 20:57:17 +0800

    working on GSC database

--- src/GSCPages/DatabasePage.js
+++ src/GSCPages/DatabasePage.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import axios from 'axios';
+import url from '../App';
 
 export default function DatabasePage() {
+  let history = useHistory();
+    const { uuid } = useParams();
+    const [gsc, setGsc] = useState();
+  
+    useEffect(() => {
+      axios.get (`${url}/gscs/${uuid}`)
+        .then((response) => {
+          setGsc(response.data)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }, [uuid])
+
+  const backToProfile = () => {
+    history.push(`/good-single-christian-friend/${uuid}`)
+    window.scrollTo(0,0)
+  }
   return (
-    <div className="color-red" style={{boxSizing:"border-box", width:"80%", margin:"50px auto"}}>
+    <div className="color-red" style={{boxSizing:"border-box", width:"90%", margin:"50px auto"}}>
+      <div className="text-align-left">
+        <button className="red-button" onClick={backToProfile} style={{padding:"5px 15px", borderRadius:"5px"}}>BACK</button>
+      </div>
+      <br />
       <h1>Monthly Database</h1>
       <p>The database will be refreshed on the 1st of every month!</p>
     </div>
