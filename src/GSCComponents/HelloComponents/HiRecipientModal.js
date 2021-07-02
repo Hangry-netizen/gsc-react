2fc9d0dd1f65ffdbdcdb8ffc18dfb662afe4cd57
Hangry <hangrylydevour@gmail.com>
2021-07-03 06:28:48 +0800

    fix remove bug in hello page said hi table

--- src/GSCComponents/HelloComponents/HiRecipientModal.js
+++ src/GSCComponents/HelloComponents/HiRecipientModal.js
@@ -53,7 +53,7 @@ export default function HiRecipientModal({ gsc, showHiRecipientModal, handleClos
       method: "POST",
       url: `${url}/hellos/remove/${gsc.hello_id}`,
       data: {
-        remove: true
+        removed: true
       }
     })
     .then((response) => {
