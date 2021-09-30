34921d8c9a69b33c1498ddf5059797ea66713a44
Hangry <hangrylydevour@gmail.com>
2021-09-30 20:43:10 +0800

    minor fixes

--- src/HomeComponents/IntroVideoMobile.js
+++ src/HomeComponents/IntroVideoMobile.js
@@ -9,7 +9,7 @@ export default function IntroVideoMobile() {
           src="https://www.youtube.com/embed/b4YhnrnWBgo?autoplay=1"
           title="Introducing MatchesUp"
           frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen; autoplay"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen;"
           allowfullscreen
         >
         </iframe>
