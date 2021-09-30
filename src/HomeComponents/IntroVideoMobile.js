5444c81c404a75086b3748752b5b8e95bb0c06ec
Hangry <hangrylydevour@gmail.com>
2021-10-01 00:42:03 +0800

    added hide related videos on intro video

--- src/HomeComponents/IntroVideoMobile.js
+++ src/HomeComponents/IntroVideoMobile.js
@@ -6,7 +6,7 @@ export default function IntroVideoMobile() {
       <div style={{boxSizing:"border-box", border:"none"}}>
         <iframe
           id="intro-video"
-          src="https://www.youtube.com/embed/b4YhnrnWBgo?autoplay=1&mute=1"
+          src="https://www.youtube.com/embed/b4YhnrnWBgo?autoplay=1&mute=1&rel=0"
           title="Introducing MatchesUp"
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen; autoplay"
